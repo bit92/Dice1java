485fb8f779eb1d04f53f56e94b025a795b36f7ec
bit92 <sahabithun92@gmail.com>
2024-10-04 14:22:51 +0200

    update

--- src/main/java/se/bithundice1java/Dice1.java
+++ src/main/java/se/bithundice1java/Dice1.java
@@ -1,30 +1,41 @@
 package se.bithundice1java;
-import java.util.Scanner;
-import java.util.Random;
+
+import java.util.Scanner; //its a class saved in Java library and we imported it here to take user input in our program
+import java.util.Random;//its a class saved in Java library and we imported it here to generate random number in our program
 
 public class Dice1 {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        Random r  = new Random();
-        // Get player names
+        Scanner sc = new Scanner(System.in); //It takes input while running the program from user
+        Random r = new Random();
+        String name;// Variable to save the name we took as input
+        int roll1, roll2;// to save the randomly generated number
+        // Set player names
         System.out.print("Enter name for Player 1: ");
-        Player player1 = new Player(sc.nextLine());
+        name = sc.nextLine();// takes name as input.
+        Player player1 = new Player(name);
 
         System.out.print("Enter name for Player 2: ");
-        Player player2 = new Player(sc.nextLine());
+        name = sc.nextLine();
+        Player player2 = new Player(name);
 
-        // Roll dice for Player 1
+        // Roll dice twice for Player 1
         System.out.println(player1.getName() + "'s turn to roll the dice.");
-
-        player1.setScore(r.nextInt(1,6)+r.nextInt(1,6));
+        roll1 = r.nextInt(6) + 1;
+        roll2 = r.nextInt(6) + 1;
+        player1.setScore(roll1 + roll2); // sum both rolls
+        System.out.println(player1.getName() + "'s roll 1: " + roll1);
+        System.out.println(player1.getName() + "'s roll 2: " + roll2);
         System.out.println(player1.getName() + "'s total score: " + player1.getScore());
 
-        // Roll dice for Player 2
+        // Roll dice twice for Player 2
         System.out.println(player2.getName() + "'s turn to roll the dice.");
-        player2.setScore(r.nextInt(1,6)+r.nextInt(1,6));
+        roll1 = r.nextInt(6) + 1;
+        roll2 = r.nextInt(6) + 1;
+        player2.setScore(roll1 + roll2); // sum both rolls
+        System.out.println(player2.getName() + "'s roll 1: " + roll1);
+        System.out.println(player2.getName() + "'s roll 2: " + roll2);
         System.out.println(player2.getName() + "'s total score: " + player2.getScore());
-
         // Determine the winner
         if (player1.getScore() > player2.getScore()) {
             System.out.println(player1.getName() + " wins!");
@@ -33,8 +44,7 @@ public class Dice1 {
         } else {
             System.out.println("It's a tie!");
         }
-
+        sc.close();
     }
 
 }
-
--- src/main/java/se/bithundice1java/Player.java
+++ src/main/java/se/bithundice1java/Player.java
@@ -1,26 +1,20 @@
-package se.bithundice1java;
+package se.bithundice1java;  // It a package  where we keep all the related classes
 
 public class Player {
     private String name;
     private int score;
 
-    public Player(String name) {
+    public Player(String name) {  //its a constructor to take the name of the player as input
         this.name = name;
     }
-
+    public String getName() {// take the value of name and print it in another class or function
+        return name;
+    }
     public void setScore(int score) {
         this.score = score;
     }
-
-    public String getName() {
-        return name;
-    }
-
     public int getScore() {
         return score;
     }
-//    public void addScore(int points) {
-//        this.score += points;
-//    }
 
 }
